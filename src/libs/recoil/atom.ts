cf10f43bc33b657ef92e05179a44ea4203ff6104
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-06-02 08:49:06 +0100

    Set up boilerplate

---
+++ src/libs/recoil/atom.ts
@@ -0,0 +1,5 @@
+import { AtomOptions, RecoilState } from "./types";
+
+export function atom<T>(options: AtomOptions<T>) {
+  return new RecoilState<T>(options.key, options.default);
+}
---
+++ src/libs/recoil/recoil-context.ts
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+import { RecoilContextState } from "./types";
+
+export const RecoilContext = createContext<RecoilContextState | null>(null);
