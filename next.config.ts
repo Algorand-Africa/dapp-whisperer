f755f9f318b6c34f2ed6b415c34ec64209485efd
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-07-01 17:05:54 +0100

    Revert next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  pageExtensions: ["page.tsx", "page.ts", "ts"],
 };
 
 export default nextConfig;
