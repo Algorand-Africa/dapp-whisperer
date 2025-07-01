d24cf634ec76c03ab22c966538b3dee17d770dab
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-07-01 17:17:29 +0100

    Revert "Downgrade from next 15 to next 14"
    This reverts commit 99463862c922d70d6e2a5c6670523dd3a6fba785.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,17 @@
 import { GlobalProvider } from "@/providers/global-provider";
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Dapp Whisperer",
@@ -14,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <GlobalProvider>{children}</GlobalProvider>
       </body>
     </html>
