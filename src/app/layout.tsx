99463862c922d70d6e2a5c6670523dd3a6fba785
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2025-07-01 17:14:50 +0100

    Downgrade from next 15 to next 14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import { GlobalProvider } from "@/providers/global-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Dapp Whisperer",
@@ -25,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`antialiased`}>
         <GlobalProvider>{children}</GlobalProvider>
       </body>
     </html>
